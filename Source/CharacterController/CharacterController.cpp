ea620bb6d17840a4d33f77a29e8d4b0fa4cca207
Santiago Gallaga-Rabinowitz <santiagogallagara@gmail.com>
2026-08-11 22:12:00 -0700

    Revert "Initial commit with UE5 and JetBrains project settings"
    This reverts commit 7781375a9009084af080d89cc9a547c3e4a86cb9.

--- Source/CharacterController/CharacterController.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CharacterController.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CharacterController, "CharacterController" );
